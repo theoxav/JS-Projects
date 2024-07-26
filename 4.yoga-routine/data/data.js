1c1f884351b99aeeb78c4e3109bf0342298edaca
theoxav <theoxxav@outlook.fr>
2024-07-26 19:19:04 +0200

    yoga project !

---
+++ 4.yoga-routine/data/data.js
@@ -0,0 +1,12 @@
+export const basicArray = [
+  { pic: 0, min: 1 },
+  { pic: 1, min: 1 },
+  { pic: 2, min: 1 },
+  { pic: 3, min: 1 },
+  { pic: 4, min: 1 },
+  { pic: 5, min: 1 },
+  { pic: 6, min: 1 },
+  { pic: 7, min: 1 },
+  { pic: 8, min: 1 },
+  { pic: 9, min: 1 },
+];
